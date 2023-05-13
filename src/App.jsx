7777cbdfeaa400631453a76acac22c5d294edfac
Umer_237 <umerafzal981@gmail.com>
2023-05-13 14:52:07 +0500

    umer

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import Contact from './pages/Contact';
 import Services from './pages/Services';
 import OurStory from './pages/OurStory';
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
 <>
 
-<BrowserRouter>
-<Routes>
-   <Route path='/' element={<Home/>} />
-   <Route path='/about' element={<About/>} />
-   <Route path='/service' element={<Services/>} />
-   <Route path='/contact' element={<Contact/>} />
-   <Route path='/ourstory' element={<OurStory/>} />
-</Routes>
-</BrowserRouter>
+<Switch>
+   <Route path='/' element={Home} />
+   <Route path='/about' element={About} />
+   <Route path='/service' element={Services} />
+   <Route path='/contact' element={Contact} />
+   <Route path='/ourstory' element={OurStory} />
+   </Switch>
   </>
     
   
