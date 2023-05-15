ecf140302d74ce5fe3858fadc7ce50298331c3d9
Umer_237 <umerafzal981@gmail.com>
2023-05-15 14:15:55 +0500

    umer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Banner from './Banner'
-import Header from './Header'
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
-import Footer from './Footer'
 
 const HomePage = () => {
   return (
