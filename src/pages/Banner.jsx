2302770be02914352bd1f46a79a71bb951e5fb9c
Umer_237 <umerafzal981@gmail.com>
2023-05-15 12:57:45 +0500

    umer

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { BsBasket } from 'react-icons/bs';
+import './Header.css';
+import { Link } from 'react-router-dom'
 import './Banner.css';
 
 function Banner() {
+  const [pages, setPages] = useState('')
+  const [change, setChange] = useState('')
   const [input, setInput] = useState("")
   const [name, setName] = useState('The Original Doughnut')
   function handleInput(event){
@@ -11,9 +16,32 @@ function Banner() {
     setInput('')
     setName(input)
   }
+  function Click () {
+    setChange('')
+    setPages('')
+  } 
   return (
    <>  
-<div className='Banner-Background'>
+   <div className='Navbar'>  
+<nav>  
+<ul>
+    <div className='Logo'>
+      <Link to='/'>
+    <BsBasket/>
+    </Link>
+    </div>
+    {/* <li><a href="/ourstory">OUR STORY</a></li> */}
+    <button onClick={Click}>OUR STORY</button>
+    {/* <li><a href="/recipes">RECIPES</a></li> */}
+    <button onClick={Click}>RECIPES</button>
+    {/* <li><a href="/contact">CONTACT</a></li> */}
+    <button onClick={Click}>CONTACT</button>
+    {/* <li><a href="/store">STORE</a></li> */}
+    <button onClick={Click}>STORE</button>
+   </ul>
+   </nav>
+   </div>
+<div className='Banner-Background' value={change}>
   <div className="Banner-Things">
        
 <h1>{name}</h1>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,28 +1,10 @@
-import React, {useState} from 'react'
-import { BsBasket } from 'react-icons/bs';
-import './Header.css';
-import { Link } from 'react-router-dom'
+import React from 'react'
 
 const Header = () => {
-  const [pages, setPages] = useState('')
-  return (   
+      
+  return (
 <>
-<div className='Navbar'>  
-<nav>  
-<ul>
-    <div className='Logo'>
-      <Link to='/'>
-    <BsBasket/>
-    </Link>
-    </div>
-    {/* <li><a href="/ourstory">OUR STORY</a></li> */}
-    <button>OUR STORY</button>
-    <li><a href="/recipes">RECIPES</a></li>
-    <li><a href="/contact">CONTACT</a></li>
-    <li><a href="/store">STORE</a></li>
-   </ul>
-   </nav>
-   </div>
+
 </>
   )
 }
