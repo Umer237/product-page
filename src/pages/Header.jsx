2d6bed939ed4e25fd59369e6aa5af1197b2f7627
Umer_237 <umerafzal981@gmail.com>
2023-05-16 12:33:41 +0500

    umer

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,36 +1,48 @@
-import React, { useState } from 'react'
-import { BsBasket } from 'react-icons/bs';
-import Banner from './Banner';
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import { BsBasket } from "react-icons/bs";
+import Banner from "./Banner";
+import "./Header.css";
 const Header = () => {
-      
-const [page, setPages] = useState('')
+  const [navLinks, setNavLinks] = useState([]);
 
-function handleupclick () {
-Banner = {page}
-setPages = {handleupclick}
-}
+  useEffect(() => {
+    const navs = [
+      { name: " OUR STORY ", path: "/ourstory" },
+      { name: " RECIPES ", path: "/recipes" },
+    ];
+
+    setNavLinks(navs);
+  }, []);
   return (
-<>
-<div className='Navbar'>  
-<nav>  
-<ul>
-    <div className='Logo'>
-<a href="/">
-    <BsBasket/>
-    </a>
-    </div>
-    {/* <li><a href="/ourstory">OUR STORY</a></li>  */}
-    <button onClick={handleupclick}>OUR STORY</button>
-    <li><a href="/recipes">RECIPES</a></li> 
-     <li><a href="/contact">CONTACT</a></li> 
-    <li><a href="/store">STORE</a></li>
-  
-   </ul>
-   </nav>
-   </div>
-</>
-  )
-}
+    <>
+      <div className="Navbar">
+        <nav>
+          {console.log(navLinks)}
+          <ul>
+            <div className="Logo">
+              <a href="/">
+                <BsBasket />
+              </a>
+            </div>
+            <li>
+              {navLinks.map((d,i) => (
+                  <li key={i}>
+              <a href={d.path}>{d.name}</a>
+            </li>
+              ))}
+            </li>
+          
+            <li>
+              <a href="/contact">CONTACT</a>
+            </li>
+            <li>
+              <a href="/store">STORE</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
